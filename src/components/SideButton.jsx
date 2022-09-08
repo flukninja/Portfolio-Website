17a2497b77aa48773d458c0a145e4ec02e953d16
Tanakorn Muwinitkul <tanakornfmu@gmail.com>
2022-09-09 02:07:38 +0700

    update resume link

--- src/components/SideButton.jsx
+++ src/components/SideButton.jsx
@@ -9,7 +9,7 @@ const SideButton = () => {
         <a href="https://www.linkedin.com/in/tanakorn-muwinitkul" target="_blank"  rel="noopener noreferrer">Linkedin <i className="fab fa-linkedin"></i></a>
         <a href="https://www.youtube.com/channel/UCcwp-goy3cM_TW3ZMJWDYQg" target="_blank"  rel="noopener noreferrer">Youtube <i className="fab fa-youtube"></i></a>
         <a href="mailto:tanakornfmu@gmail.com" target="_blank"  rel="noopener noreferrer">Email <i className="fas fa-envelope"></i></a>
-        <a href="https://drive.google.com/file/d/1EBBMqaD8wXktcuHDeEdMWFZh4Q3nEFMV/view?usp=sharing" target="_blank"  rel="noopener noreferrer">Resume <i className="far fa-file"></i></a>
+        <a href="https://drive.google.com/file/d/1XjJLvQJBOuCmeb4MeZhou4jnj03oNjPi/view?usp=sharing" target="_blank"  rel="noopener noreferrer">Resume <i className="far fa-file"></i></a>
     </div>
   )
 }
